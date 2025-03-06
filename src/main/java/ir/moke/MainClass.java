90845d324f583f4cde56d1361fd4592b6a0aa0f7
Mahdi <mah454@mail.moke.ir>
2025-03-06 14:08:05 +0330

    Improvement Code

--- src/main/java/ir/moke/MainClass.java
+++ src/main/java/ir/moke/MainClass.java
@@ -1,52 +1,8 @@
 package ir.moke;
 
-import ir.moke.filter.CORSFilter;
-import ir.moke.filter.SampleFilter;
-import ir.moke.servlet.SampleServlet;
-import ir.moke.ws.SampleWebSocket;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.websocket.server.WsSci;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class MainClass {
-    private static final String contextPath = "";
-    private static final String baseDir = "/tmp/tomcat";
-
-    static {
-        try {
-            Files.createDirectory(Path.of(baseDir));
-        } catch (IOException ignore) {
-        }
-    }
 
     public static void main(String[] args) throws Exception {
-
-        var tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        tomcat.getConnector().setProperty("address", "0.0.0.0");
-
-        var context = tomcat.addWebapp(contextPath, baseDir);
-
-        // add websockets
-        context.addServletContainerInitializer(new WsSci(), new HashSet<>(List.of(SampleWebSocket.class)));
-
-        // add servlets
-        context.addServletContainerInitializer(new EmbeddedServletContainerInitializer(), Set.of(SampleServlet.class));
-
-        /*
-         * add filters
-         * Note : Filter could be ordered by @WebFilter#filterName method
-         * */
-        context.addServletContainerInitializer(new EmbeddedFilterContainerInitializer(), Set.of(SampleFilter.class, CORSFilter.class));
-
-        tomcat.start();
-        System.out.println("Tomcat startup is completed, listening to port 8080");
-        tomcat.getServer().await();
+        HttpContainer.start();
     }
 }
